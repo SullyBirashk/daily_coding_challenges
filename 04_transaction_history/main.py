7d9fcbf6a3c6b01838e96e00c0aefc3384e4a03f
Sully's Github <sully.birashk@gmail.com>
2025-03-19 22:41:33 -0700

    Initial Commit

---
+++ 04_transaction_history/main.py
@@ -0,0 +1,5 @@
+## Challenge of the day ## 
+# Modify the `BankAccount` class to track transaction history.
+# Each transaction should record the type ('deposit' or 'withdraw'),
+# the amount, and the updated balance after the transaction.
+# Add a method to retrieve and display the full transaction history.
