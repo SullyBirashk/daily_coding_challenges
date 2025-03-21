9fef7e122dd994bbffb317ffa13ef6fbd75af2e4
Sully's Github <sully.birashk@gmail.com>
2025-03-20 19:47:25 -0700

    Initial Commit

--- 10_abstract_bank_account/main.py
+++ 10_abstract_bank_account/main.py
@@ -2,4 +2,8 @@
 # Implement an abstract `BankAccount` class.
 # This class should define a blueprint for different types of accounts
 # by using abstract methods that must be implemented by subclasses.
+# Create `SavingsAccount` and `CheckingAccount` as concrete subclasses.## Challenge of the day ## 
+# Implement an abstract `BankAccount` class.
+# This class should define a blueprint for different types of accounts
+# by using abstract methods that must be implemented by subclasses.
 # Create `SavingsAccount` and `CheckingAccount` as concrete subclasses.
